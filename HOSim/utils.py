d5745bcc2fa57cf611bf7d469dafd55b84062b73
Vegard Bårsaune <vegabaar@stud.ntnu.no>
2025-04-24 14:48:45 +0200

    Fixed initital conditions

--- HOSim/utils.py
+++ HOSim/utils.py
@@ -9,28 +9,25 @@ def get_initial_condition(params):
     length = params["length"]
     modes = params["modes"]
 
-    fp = 1 / Tp
     k0 = 2 * np.pi / length
+    k = np.arange(1, modes+1)*k0
+    w = np.sqrt(k * g)
 
-    f = np.arange(1, modes+1)*k0
-    sigma = 0.07 * (f <= fp) + 0.09 * (f > fp)
-    Sf = (g**2 / (2 * np.pi)**4) * f**(-5) * \
-        np.exp(-1.25 * (fp / f)**4) * \
-        gamma**np.exp(-((f / fp - 1)**2) / (2 * sigma**2))
+    T1 = 0.834 * Tp
+    T2 = Tp / 1.408
 
-    alpha = Hs**2 / 16 / np.trapezoid(Sf, f)
-    Sf = alpha * Sf
+    sigma = 0.07 * (w <= 5.24/T1) + 0.09 * (w > 5.24/T1)
+    gamma = 3.3
+    Sw = (155 * Hs**2 / T1**4 / w**5 *
+        np.exp(-944 / (T1*w)**4) *
+        gamma ** np.exp(-((0.191*w*T1 - 1)/(np.sqrt(2)*sigma))**2))
+    Sk = Sw * g / (2 * np.sqrt(k * g))
 
-    f = np.insert(f, 0, 0)
-    Sf = np.insert(Sf, 0, 0)
+    phases = np.exp(np.random.rand(modes)*2*np.pi*1.j)
+    eta_hat = np.sqrt(Sk * k0 * 0.5) * phases * 2 * modes
+    phi_hat = eta_hat * np.exp(-1.j * np.pi / 2) * np.sqrt(g / k)
 
-    Sf_max = alpha * g**2 * (2 * np.pi)**(-4) * fp**(-5) * np.exp(-5/4) * gamma
-
-    assert Sf[-1]/Sf_max <= 0.01, "Not enough modes to represent seastate!"
-
-    phases = np.exp(np.random.rand(modes+1)*2*np.pi*1.j)
-    eta_hat = np.sqrt(Sf * k0 * 0.5) * phases * 2 * modes
-    phi_hat = eta_hat[1:] * np.exp(-1.j * np.pi / 2) * np.sqrt(g / f[1:])
+    eta_hat = np.insert(eta_hat, 0, 0)
     phi_hat = np.insert(phi_hat, 0, 0)
 
     return np.concatenate((eta_hat, phi_hat))
